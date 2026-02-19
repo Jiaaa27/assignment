5a6f6d4ff32ddf9f5bfd64db88561adad1084e28
Jiaaa27 <148185000+Jiaaa27@users.noreply.github.com>
2026-02-19 15:17:18 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+// FILE: server.js
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 5000;
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
