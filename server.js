3e53c1f92e70ba55dfed4f64d38a3e14361e0da7
Jiaaa27 <148185000+Jiaaa27@users.noreply.github.com>
2026-02-19 15:23:17 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
-// FILE: server.js
 const express = require("express");
-const path = require("path");
+const os = require("os");
+const cors = require("cors");
 
 const app = express();
-const PORT = 5000;
+app.use(cors());
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, "client/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+// Free Memory
+app.get("/api/freememory", (req, res) => {
+  res.json({ freeMemory: os.freemem() });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+// Total Memory
+app.get("/api/totalmemory", (req, res) => {
+  res.json({ totalMemory: os.totalmem() });
+});
+
+// CPU Architecture
+app.get("/api/cpuarch", (req, res) => {
+  res.json({ cpuArchitecture: os.arch() });
+});
+
+// User Info
+app.get("/api/userinfo", (req, res) => {
+  res.json({ userInfo: os.userInfo() });
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
 });
